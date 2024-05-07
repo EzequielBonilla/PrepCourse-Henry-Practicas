6172f3e835b586f37a6ccfc0c2b5dfda37e6f986
Ezequiel Bonilla <ezequiel.bonilla92@gmail.com>
2024-05-07 18:31:16 -0300

    primeros archivos js
    primeros archivos js

---
+++ C3/operadores y precedencia.js
@@ -0,0 +1,21 @@
+var nombre = "Ezequiel"
+var saludo = "Hola " + nombre;
+console.log(saludo)
+
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
---
+++ C3/variables.js
@@ -0,0 +1,23 @@
+//String representa datos textuales
+
+var bootcamp = "henry"
+
+//Numeros representa valores numericos
+
+var numeroEntero = 17;
+var numeroNegativo = -4;
+var numeroDecimal = 10.45;
+
+//Booleanos representa dos valores logicos
+
+var verdadero = true;
+var falso = false;
+
+//Undefined representa variable no declarada o sin valor asignado
+
+var cajonSinValorDefinido
+console.log(cajonSinValorDefinido);
+
+//Null representa variable intencionalmente sin valor, es decir nulo o "vacio"
+
+var cajonSinValorIntencional = null;
