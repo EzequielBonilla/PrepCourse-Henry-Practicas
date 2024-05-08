df6ed1e753af8176d610bf0f398cf744ca2c10fb
EzequielBonilla <ezequiel.bonilla92@gmail.com>
2024-05-07 22:44:44 -0300

    C5 completado sin Hmwk

---
+++ C3/funciones2.js
@@ -0,0 +1,8 @@
+function cuidado(nombre) {
+    console.log(nombre);
+    return nombre;
+}
+
+function returnNombre() {
+    return ("El nombre retornado por la funcion 'cuidado' es: " + cuidado ("Ezequiel"))
+}
