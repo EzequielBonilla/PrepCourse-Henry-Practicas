374ab148f7e41b12def8905e5a83b1bc3fd7d620
EzequielBonilla <ezequiel.bonilla92@gmail.com>
2024-05-15 23:12:28 -0300

    por empezar hm7

--- bucles con arrays.js
+++ bucles con arrays.js
@@ -3,7 +3,21 @@
 //     console.log(arr[i])
 // };
 
-function encontrarLetraP(string) {
-    var letras = string.split("");
-    
-} 
+// function encontrarLetraP(string) {
+//     var letras = string.split(""); //para recorrer el string, lo separamos en un array
+
+//     for(let i = 0; i<letras.length; i++) {
+//         if (letras[i]==="p") {
+//             console.log("Si contiene a P"); 
+//         } 
+//     }
+// } 
+
+// encontrarLetraP("Javascript")
+// encontrarLetraP("Henry")
+
+var arr=[];
+while (arr.length <5) {
+    arr.push("BOOM");
+}
+console.log(arr)
