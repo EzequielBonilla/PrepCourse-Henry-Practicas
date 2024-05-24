279d5f4768cf800f1bf5ce944a84e97401245917
Ezequiel Bonilla <ezequiel.bonilla92@gmail.com>
2024-05-24 18:34:13 -0300

    objetos

---
+++ objets.js
@@ -0,0 +1,16 @@
+var deportes = {
+    conPelota: ["futbol", "basquet", "rugby"],
+    sinPelota: ["boxeo", "surf", "trekking"],
+    // todos los objetos respetan la regla par calve: valor o key: value
+};
+
+var persona = {
+    nombre: "Lucas",
+    edad: 26,
+    estudios: {
+        esProgramador: true
+    }
+};
+console.log(persona.edad)
+
+// tenemos Dot-Notation y Bracket-Notation
